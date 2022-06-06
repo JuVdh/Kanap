a378636a85afe2c891f655d84b3bdded77ba6534
JuVdh <julie.vdh@orange.fr>
2022-06-07 01:40:39 +0200

    cart.js simplification code et noms variables, recup, validation et envoie données contact

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,4 +1,4 @@
-let emplacementPanier=document.getElementById("cart__items");
+let panier=document.getElementById("cart__items");
 let cartQuantity=document.getElementById("totalQuantity");
 let cartPrice=document.getElementById("totalPrice");
 let dataPanier=JSON.parse(localStorage.getItem("data-panier"));
@@ -11,39 +11,43 @@ function getProductPanier(articlePanier){
 
 function insertArticlePanier(productPanier,articlePanier){   
 
-    let panier=emplacementPanier.appendChild(document.createElement("article"));
-    panier.classList.add("cart__item");
-    let emplacementImage=panier.appendChild(document.createElement("div"));
-    emplacementImage.classList.add("cart__item__img");
-    let imagePanier=emplacementImage.appendChild(document.createElement("img")); 
-    imagePanier.setAttribute("src",productPanier.imageUrl);
-    imagePanier.setAttribute("alt",productPanier.altTxt); 
-    let emplacementContenu=panier.appendChild(document.createElement("div"));
-    emplacementContenu.classList.add("cart__item__content");
-    let emplacementContenuDescription=emplacementContenu.appendChild(document.createElement("div"));
-    emplacementContenuDescription.classList.add("cart__item__content__description");
-    articlePanierNom=emplacementContenuDescription.appendChild(document.createElement("h2"));
-    articlePanierNom.textContent=productPanier.name;
-    articlePanierCouleur=emplacementContenuDescription.appendChild(document.createElement("p"));
-    articlePanierCouleur.textContent=articlePanier.color;
-    articlePanierPrix=emplacementContenuDescription.appendChild(document.createElement("p"));
-    articlePanierPrix.textContent=productPanier.price+" €";
-    let emplacementContenuSettings=emplacementContenu.appendChild(document.createElement("div"));
-    emplacementContenuSettings.classList.add("cart__item__content__settings");
-    let emplacementContenuSettingsQuantite=emplacementContenuSettings.appendChild(document.createElement("div"));
-    emplacementContenuSettingsQuantite.classList.add("cart__item__content__settings__quantity");
-    articlePanierQuantite=emplacementContenuSettingsQuantite.appendChild(document.createElement("p"));
-    articlePanierQuantite.textContent="Qté : ";
-    let inputQuantite=emplacementContenuSettingsQuantite.appendChild(document.createElement("input"));
+    let article=panier.appendChild(document.createElement("article"));
+    article.classList.add("cart__item");
+
+    let articleImage=article.appendChild(document.createElement("div"));
+    articleImage.classList.add("cart__item__img");
+
+    let image=articleImage.appendChild(document.createElement("img")); 
+    image.setAttribute("src",productPanier.imageUrl);
+    image.setAttribute("alt",productPanier.altTxt); 
+
+    let contenu=article.appendChild(document.createElement("div"));
+    contenu.classList.add("cart__item__content");
+    
+    let description=contenu.appendChild(document.createElement("div"));
+    description.classList.add("cart__item__content__description");
+    description.appendChild(document.createElement("h2")).textContent=productPanier.name;
+    description.appendChild(document.createElement("p")).textContent=articlePanier.color;
+    description.appendChild(document.createElement("p")).textContent=productPanier.price+" €";
+    
+    let settings=contenu.appendChild(document.createElement("div"));
+    settings.classList.add("cart__item__content__settings");
+
+    let quantite=settings.appendChild(document.createElement("div"));
+    quantite.classList.add("cart__item__content__settings__quantity");
+    quantite.appendChild(document.createElement("p")).textContent="Qté : ";
+    
+    let inputQuantite=quantite.appendChild(document.createElement("input"));
     inputQuantite.classList.add("itemQuantity");
     inputQuantite.setAttribute("type","number");
     inputQuantite.setAttribute("name","inputQuantity");
     inputQuantite.setAttribute("min","1");
     inputQuantite.setAttribute("max","100");
     inputQuantite.setAttribute("value",articlePanier.quantity);
-    let emplacementContenuSettingsDelete=emplacementContenuSettings.appendChild(document.createElement("div"));
-    emplacementContenuSettingsDelete.classList.add("cart__item__content__settings__delete");
-    articlePanierDelete=emplacementContenuSettingsDelete.appendChild(document.createElement("p"));
+
+    let deleteSettings=settings.appendChild(document.createElement("div"));
+    deleteSettings.classList.add("cart__item__content__settings__delete");
+    articlePanierDelete=deleteSettings.appendChild(document.createElement("p"));
     articlePanierDelete.classList.add("deleteItem");
     articlePanierDelete.textContent="Supprimer";
 
@@ -59,8 +63,8 @@ function insertArticlePanier(productPanier,articlePanier){
                 console.log(dataPanier[i]);
                 dataPanier.splice(i,1);
                 updateTotal(dataPanier);
-                panier.innerHTML='';
-                //emplacementPanier.innerHTML='';
+                article.innerHTML='';
+                //panier.innerHTML='';
                 //cartDisplay(dataPanier);
                 break;
             }
@@ -68,18 +72,18 @@ function insertArticlePanier(productPanier,articlePanier){
     })
 }
 
-function cartDisplay(panier){
-    for (let articlePanier of panier){
+function cartDisplay(monPanier){
+    for (let articlePanier of monPanier){
         getProductPanier(articlePanier).then(product=>{
              insertArticlePanier(product, articlePanier);
         })
     }
 }
 
-function updateTotal(panier){
+function updateTotal(monPanier){
     let totalPrice=0;
     let totalQuantity=0;
-    for (let articlePanier of panier){
+    for (let articlePanier of monPanier){
         getProductPanier(articlePanier).then(product=>{
             totalQuantity+= articlePanier.quantity;  
             cartQuantity.textContent=totalQuantity;
@@ -89,12 +93,179 @@ function updateTotal(panier){
     }
  }
 
-function cartUpdate(panier){
-    cartDisplay(panier);
-    updateTotal(panier);
+function cartUpdate(monPanier){
+    cartDisplay(monPanier);
+    updateTotal(monPanier);
 }
  
 cartUpdate(dataPanier);
+localStorage.setItem("data-panier",JSON.stringify(dataPanier));
 
 
+/*******validation données utilisateurs *********/
+let dataContact=JSON.parse(localStorage.getItem('contact')) ?? [];
 
+function firstNameIsValid(value) {
+  return /^[A-Za-z\s'-]{2,}$/.test(value);
+}
+
+function lastNameIsValid(value) {
+  return /^[A-Za-z\s'-]{2,}$/.test(value);
+}
+
+function addressIsValid(value) {
+  return /^[0-9]*[a-zA-Z\s,'-]{3,}$/.test(value);
+}
+
+function cityIsValid(value) {
+  return /^[0-9]{1}[1-9]{1}[0-9]{3}[a-zA-Z\s,'-]{2,}$/.test(value);
+}
+
+function emailIsValid(value) {
+  return /^[a-zA-Z0-9-_\.]+@[a-zA-Z0-9-_\.]+\.[a-zA-Z]{2,}$/.test(value);
+}
+
+function getFirstNameErrMsg(){
+    return document.getElementById("firstNameErrorMsg");
+}
+function getLastNameErrMsg(){
+    return document.getElementById("lastNameErrorMsg");
+}
+
+function getAddressErrMsg(){
+    return document.getElementById("addressErrorMsg");
+}
+
+function getCityErrMsg(){
+    return document.getElementById("cityErrorMsg");
+}
+
+function getEmailErrMsg(){
+    return document.getElementById("emailErrorMsg");
+}
+
+function disableSubmit(disabled) {
+    if (disabled) {
+      document
+        .getElementById("order")
+        .setAttribute("disabled", true);
+    } else {
+      document
+        .getElementById("order")
+        .removeAttribute("disabled");
+    }
+  }
+
+document
+  .getElementById("firstName")
+  .addEventListener("change", function(e) {
+  if (firstNameIsValid(e.target.value)) {
+    getFirstNameErrMsg().innerText = "ok";
+    disableSubmit(false);
+    firstName=e.target.value;
+   
+  } else {
+    getFirstNameErrMsg().innerText = "veuillez entrer au moins 2 lettres, les chiffres ne sont pas acceptés";
+    disableSubmit(true);
+  }
+});
+
+document
+  .getElementById("lastName")
+  .addEventListener("change", function(e) {
+  if (lastNameIsValid(e.target.value)) {
+    getLastNameErrMsg().innerText = "ok";
+    disableSubmit(false);
+    lastName=e.target.value;
+    
+  } else {
+    getLastNameErrMsg().innerText = "veuillez entrer au moins 2 lettres, les chiffres ne sont pas acceptés";
+    disableSubmit(true);
+  }
+});
+
+document
+  .getElementById("address")
+  .addEventListener("change", function(e) {
+  if (addressIsValid(e.target.value)) {
+    getAddressErrMsg().innerText = "ok";
+    disableSubmit(false);
+    address=e.target.value;
+    
+  } else {
+    getAddressErrMsg().innerText = "veuillez entrer le numéro en chiffres suivi du nom de la voie";
+    disableSubmit(true);
+  }
+});
+
+document
+  .getElementById("city")
+  .addEventListener("change", function(e) {
+  if (cityIsValid(e.target.value)) {
+    getCityErrMsg().innerText = "ok";
+    disableSubmit(false);
+    city=e.target.value;
+    
+  } else {
+    getCityErrMsg().innerText = "veuillez entrer le code postal (5 chiffres) suivi du nom de la ville";
+    disableSubmit(true);
+  }
+});
+
+document
+  .getElementById("email")
+  .addEventListener("change", function(e) {
+  if (emailIsValid(e.target.value)) {
+    getEmailErrMsg().innerText = "ok";
+    disableSubmit(false);
+    email=e.target.value;
+    
+  } else {
+    getEmailErrMsg().innerText = "veuillez entrer une adresse email valide";
+    disableSubmit(true);
+  }
+});
+
+function send(e){
+  e.preventDefault();
+  dataContact.push({
+    prenom:firstName,
+    nom:lastName,
+    adresse:address,
+    ville:city,
+    adresse_mail:email
+});
+fetch("http://localhost:3000/api/products/order", {
+    method: "POST",
+    headers: {
+      'Accept': 'application/json', 
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(dataContact)
+  }) 
+  .then(function(res) {
+    if (res.ok) {
+      return res.json();
+    }
+  }) 
+}
+
+document
+  .getElementById("order")
+  .addEventListener("submit", send)
+    
+
+
+/* document
+  .getElementById("firstName")
+  .addEventListener("input", function(e) {
+  if (/^[A-Za-z\s'-]{2,}$/.test(e.target.value)) {
+    getFirstNameErrMsg().innerText = "ok";
+    disableSubmit(false);
+   
+  } else {
+    getFirstNameErrMsg().innerText = "veuillez entrer au moins 2 lettres, les chiffres ne sont pas acceptés";
+    disableSubmit(true);
+  }
+});
+ */
